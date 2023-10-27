308cecd6e256b35360cbb98227a1fbcad6878f62
KevinArias1 <kevinstiwar828@gmail.com>
2023-10-27 16:55:56 -0700

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A program that prints a linewith function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
