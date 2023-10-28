8883663cc70d61acb250578de5f649a7190ebac7
KevinArias1 <kevinstiwar828@gmail.com>
2023-10-27 17:07:36 -0700

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * main -A program that print a line using the print funtion
+ * Return 0 (success)
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
