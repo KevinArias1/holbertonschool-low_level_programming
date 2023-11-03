48037e3b1d7daf6acee48c69f0705d0017169c3d
KevinArias1 <kevinstiwar828@gmail.com>
2023-11-02 20:56:01 -0700

    1

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,26 +1,24 @@
-#include <stdio.h> 
-/**
- *  * main - hexadecimal
- *   * Return: always return 0
- *    */
+#include<stdio.h>
 
+/**
+ *  *main - Program that prints all the numbers of base 16 in lowercase
+ *   *Return: Always 0.
+ *    */
 int main(void)
 {
-	char c;
+	int n;
+	char letters;
 
-	int d;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
 
-	c= 'a';
-	d= 0;
-	while
-		(d < 10) {
-			putchar(d + '0');
-		}
-	while
-		(c <= 'f') {
-			putchar(c);
-			c++;
-		}
+	for (letters = 'a'; letters <= 'f'; letters++)
+	{
+		putchar(letters);
+	}
+
 	putchar('\n');
 	return (0);
 }
