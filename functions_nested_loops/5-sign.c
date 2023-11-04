ad1ee57c58bd9aefb259d3719ec1f2a0bbbe9539
KevinArias1 <kevinstiwar828@gmail.com>
2023-11-03 21:31:09 -0700

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_sing - print sing of a number
  * @n - funtion parametre
@@ -7,43 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 			{
-			_putchar('0');
+			_putchar(0);
 			return (0);
 			}
 	else
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
-}
-int main(void)
-{
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 }
